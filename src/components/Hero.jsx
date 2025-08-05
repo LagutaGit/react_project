19d63f684fca025b8a6f51a6ee50b499a354de2f
LagutaGit <roma_laguta@mail.ru>
2025-08-05 15:06:06 +0900

    Дорабатываю кнопку

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRef } from 'react';
 import { useState } from 'react'
+import Button from './Button';
 
 const Hero = () => {
 
@@ -59,20 +60,29 @@ const Hero = () => {
 
                 <video 
                     src={getVideoSrc(currentIndex === totalVideos - 1 ? 1: currentIndex)}
-                    autoPlay
+                   // autoPlay
                     loop
                     muted
                     className='absolute left-0 top-0 size-full object-cover object-center'
                     onLoadedData={handleVideoLoad}
                 />
             </div>
-            <h1 className='special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75'>
-                G <b>a</b>ming
+            <h1 className='font-zentry hero-heading absolute bottom-5 right-5 z-40 text-blue-50'>
+                Gaming
             </h1>
-            <div>
-                <div>
-                    <h1></h1>
-                    <p></p>
+            <div className='absolute left-0 top-0 z-40 size-full'>
+                <div className='mt-24 px-5 sm:px-10'>
+                    <h1 className='special-font hero-heading text-blue-100'>REDEFINE</h1>
+                    <p className='mb-5 max-w-64 font-robert-regular text-blue-100'>
+                        Enter te Metagame layer <br /> Unleash th Economy
+                    </p>
+
+                    <Button 
+                    id="watch-trailer" 
+                    title="Watch trailer" 
+                    //leftIcon={<TiLocationArrow />} 
+                    containerClass = "bg-yellow-300 flex-center gap-1"
+                    />
                 </div>
             </div>
             
