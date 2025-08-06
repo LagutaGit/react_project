ac336dd942a0438d75ccbe1060b352e3336e653e
LagutaGit <roma_laguta@mail.ru>
2025-08-06 15:30:17 +0900

    Сделал обработку видео, доработал компонент VideoPreview, Hero, About. Начал делать AnimatedTitle

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Hero from './components/Hero'
+import About from './components/About'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <main className='relative min-h-screen w-screen overflow-x-hidden'> 
+      <Header />
       <Hero />
+      <About />
     </main> 
+    
     
   )
 }
